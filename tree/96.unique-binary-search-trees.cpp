4b69f85bccfd4d8efb83d9629217c7106952700b
koritafei <koritafei@gmail.com>
2021-05-10 16:26:33 +0800

    leetcode

--- tree/96.unique-binary-search-trees.cpp
+++ tree/96.unique-binary-search-trees.cpp
@@ -6,46 +6,55 @@
  * https://leetcode.com/problems/unique-binary-search-trees/description/
  *
  * algorithms
- * Medium (54.16%)
- * Likes:    4374
- * Dislikes: 162
- * Total Accepted:    351.4K
- * Total Submissions: 645K
+ * Medium (54.59%)
+ * Likes:    4458
+ * Dislikes: 164
+ * Total Accepted:    354.7K
+ * Total Submissions: 649.7K
  * Testcase Example:  '3'
  *
  * Given an integer n, return the number of structurally unique BST's (binary
  * search trees) which has exactly n nodes of unique values from 1 to n.
- * 
- * 
+ *
+ *
  * Example 1:
- * 
- * 
+ *
+ *
  * Input: n = 3
  * Output: 5
- * 
- * 
+ *
+ *
  * Example 2:
- * 
- * 
+ *
+ *
  * Input: n = 1
  * Output: 1
- * 
- * 
- * 
+ *
+ *
+ *
  * Constraints:
- * 
- * 
+ *
+ *
  * 1 <= n <= 19
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 class Solution {
 public:
-    int numTrees(int n) {
-        
+  int numTrees(int n) {
+    std::vector<int> dp(n + 1, 0);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+      for (int j = 0; i < i; j++) {
+        dp[i] += dp[j] * dp[i - j + 1];
+      }
     }
+
+    return dp[n];
+  }
 };
 // @lc code=end
-
