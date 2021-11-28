2aa8ac92611ac7281d4397951f5db533e29f876d
koritafei <koritafei@gmail.com>
2021-11-28 16:45:33 +0800

    RB Tree

--- tree/rb_tree.cc
+++ tree/rb_tree.cc
@@ -42,10 +42,6 @@ private:
   void removeFixUp(RBNode<T> *&root, RBNode<T> *node, RBNode<T> *parent);
 
   RBNode<T> *search(RBNode<T> *root, T key) const;
-  void       print(RBNode<T> *root) const;
-  void       preOrder(RBNode<T> *root) const;
-  void       inOrder(RBNode<T> *root) const;
-  void       postOrder(RBNode<T> *root) const;
 
   RBNode<T> *root;
 };
@@ -235,4 +231,190 @@ void RBTree<T>::destory(RBNode<T> *root) {
 
   delete root;
   root = nullptr;
+}
+
+// 红黑树删除
+template <typename T>
+void RBTree<T>::remove(T key) {
+  RBNode<T> *deletenode = search(key);
+
+  if (nullptr != deletenode) {
+    remove(root, deletenode);
+  }
+}
+
+template <typename T>
+void RBTree<T>::remove(RBNode<T> *&root, RBNode<T> *node) {
+  RBNode<T> *child, *parent;
+  RBColor    color;
+
+  // 被删除节点为非叶子结点
+  if (nullptr != node->left && nullptr != node->right) {
+    RBNode<T> *replace = node;
+
+    // 查找后继节点，右子树的最左节点
+    replace = node->right;
+    while (replace->left) {
+      replace = replace->left;
+    }
+
+    // 被删节点不为根节点的情况
+    if (nullptr != node->parent) {
+      if (node == node->parent->left) {
+        node->parent->left = replace;
+      } else {
+        node->parent->right = replace;
+      }
+    } else {
+      // 根节点的情况
+      root = replace;
+    }
+
+    // child是后续节点的右节点，是需要调整的节点
+    // replace是后继节点，不可能存在左节点
+    //同理前驱节点不可能存在右节点
+    child  = replace->right;
+    parent = replace->parent;
+    color  = replace->color;
+
+    // 被删节点时取代节点的父节点的情况
+    if (node == parent) {
+      parent = replace;
+    } else {
+      // child节点存在的情况
+      if (nullptr != child) {
+        child->parent = parent;
+      }
+
+      parent->left        = child;
+      replace->right      = node->right;
+      node->right->parent = replace;
+    }
+
+    replace->parent    = node->parent;
+    replace->color     = node->color;
+    replace->parent    = node->parent;
+    node->left->parent = replace;
+
+    if (BLACK == color) {
+      removeFixUp(root, replace, parent);
+    }
+
+    delete node;
+    return;
+  }
+
+  // 被删除节点只有左节点的情况，找到被删除节点的子节点
+  if (nullptr != node->left) {
+    child = node->left;
+  } else {
+    child = node->right;
+  }
+
+  parent = node->parent;
+  color  = node->color;
+
+  if (child) {
+    child->parent = parent;
+  }
+
+  // 被删除节点不为根节点
+  if (nullptr != parent) {
+    if (parent->left == node) {
+      parent->left = child;
+    } else {
+      parent->right = child;
+    }
+  } else {
+    RBTree::root = child;
+  }
+
+  if (BLACK == color) {
+    removeFixUp(root, child, parent);
+  }
+
+  return;
+}
+
+template <typename T>
+void RBTree<T>::removeFixUp(RBNode<T> *&root,
+                            RBNode<T>  *node,
+                            RBNode<T>  *parent) {
+  RBNode<T> *othernode;
+  while (!node || BLACK == node->color && RBTree::root != node) {
+    if (node == parent->left) {
+      othernode = parent->right;
+      if (RED == othernode->color) {
+        othernode->color = BLACK;
+        parent->color    = RED;
+        leftRotate(root, parent);
+        othernode = parent->right;
+      } else {
+        if (!othernode->right || BLACK == othernode->right->color) {
+          othernode->left->color = BLACK;
+          othernode->color       = RED;
+          rightRotate(root, othernode);
+          othernode = parent->right;
+        }
+
+        othernode->color        = parent->color;
+        parent->color           = BLACK;
+        othernode->right->color = BLACK;
+        leftRotate(root, parent);
+        node = root;
+        break;
+      }
+    } else {
+      othernode = parent->left;
+      if (RED == othernode->color) {
+        othernode->color = BLACK;
+        parent->color    = RED;
+        rightRotate(root, parent);
+        othernode = parent->left;
+      }
+
+      if ((!othernode->left || BLACK == othernode->left->color) &&
+          (!othernode->right || BLACK == othernode->right->color)) {
+        othernode->color = RED;
+        node             = parent;
+        parent           = node->parent;
+      } else {
+        if (!othernode->left || othernode->left->color == BLACK) {
+          othernode->right->color = BLACK;
+          othernode->color        = RED;
+          leftRotate(root, othernode);
+          othernode = parent->left;
+        }
+
+        othernode->color       = parent->color;
+        parent->color          = BLACK;
+        othernode->left->color = BLACK;
+        rightRotate(root, parent);
+        node = root;
+        break;
+      }
+    }
+  }
+
+  if (node) {
+    node->color = BLACK;
+  }
+}
+
+template <typename T>
+RBNode<T> *RBTree<T>::search(T key) {
+  return search(root, key);
+}
+
+template <typename T>
+RBNode<T> *RBTree<T>::search(RBNode<T> *root, T key) const {
+  if (nullptr == root || root->key == key) {
+    return root;
+  }
+
+  if (root->key < key) {
+    return search(root->right, key);
+  } else {
+    return search(root->left, key);
+  }
 }
